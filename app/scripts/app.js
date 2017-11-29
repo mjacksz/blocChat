d183aa2b352a7801782be54a67ad29b781606c19
Martin Jackson <l3tmein.6@gmail.com>
2017-11-29 04:07:32 -0800

    Ckpt 2: Finished

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,7 +8,7 @@
  
          $stateProvider
              .state('home', {
-                 url: '/home',
+                 url: '/',
                  controller: 'HomeCtrl as home',
                  templateUrl: '/templates/home.html'
              });
