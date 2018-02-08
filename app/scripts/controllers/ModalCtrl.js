0d8efb6b64242e9e3780eb2731023cca121be2f5
Martin Jackson <l3tmein.6@gmail.com>
2018-02-08 15:30:37 -0800

    blocChat-3 close to done

--- app/scripts/controllers/ModalCtrl.js
+++ app/scripts/controllers/ModalCtrl.js
@@ -2,22 +2,16 @@
     function ModalCtrl(Room, $uibModalInstance) {
         console.log("Inside ModalCtrl.js " + Room );
         this.Rooms = Room.all;
-        // we'll use this in our `ng-model` for the text input in the view
        
-        // add a room.hjere
   this.ok = function() {
-    console.log(" ModalCtrl.js $uibModalInstance.close " + Room );
+    console.log("At the this.ok " + this.searchTerm );
+    Room.add(this.searchTerm);
     $uibModalInstance.close();
   };
-    
-   this.submit = function() {
-    console.log(" ModalCtrl.js $uibModalInstance.submit " + Room );
-    $uibModalInstance.submit();
-  };     
-
+      
   this.cancel = function() {
     console.log(" ModalCtrl.js $uibModalInstance.cancel");
-    $uibModalInstance.cancel();
+    $uibModalInstance.close();
     // $uibModalInstance.dismiss('cancel');
   };
 }
